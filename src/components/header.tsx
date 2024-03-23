4fd56d9b50cc8a42996fdd913ed33691b82a9ba0
Vitor Souza da Silva <39885926+Vitor545@users.noreply.github.com>
2024-03-23 23:41:12 +0000

    adiciona correção header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,40 +1,42 @@
-import * as React from 'react';
-import { HamburgerMenuIcon } from '@radix-ui/react-icons';
-import { Input } from "@/components/ui/input"
-import { ModeToggle } from './mode_toggle';
+import * as React from "react";
+import { HamburgerMenuIcon } from "@radix-ui/react-icons";
+import { Input } from "@/components/ui/input";
+import { ModeToggle } from "./mode_toggle";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
 
-import Link from 'next/link';
-import { Button } from './ui/button';
+import Link from "next/link";
+import { Button } from "./ui/button";
 
-
-export interface IHeaderProps {
-}
+export interface IHeaderProps {}
 
 export default function Header(props: IHeaderProps) {
   return (
-    <header className='w-full h-20 border-0 border-b-2 border-blend-darken fixed top-0 left-0 bg-background'>
-      <div className='px-6 flex items-center h-full justify-between'>
-        <div className='flex items-center'>
-          <HamburgerMenuIcon className='w-4 h-4 mr-4 cursor-pointer' />
-          <Link href={'/'}>
-            <h5 className='font-semibold'>SIEF</h5>
+    <header className="z-[9000] w-full h-20 border-0 border-b-2 border-blend-darken fixed top-0 left-0 bg-background">
+      <div className="px-6 flex items-center h-full justify-between">
+        <div className="flex items-center">
+          <HamburgerMenuIcon className="w-4 h-4 mr-4 cursor-pointer" />
+          <Link href={"/"}>
+            <h5 className="font-semibold">SIEF</h5>
           </Link>
         </div>
-        <div className='flex items-center gap-4'>
+        <div className="flex items-center gap-4">
           <Input placeholder="Pesquisar" />
           <ModeToggle />
           <ClerkLoading>
-            <HamburgerMenuIcon className='h-5 w-5 text-muted-foreground animate-spin' />
+            <HamburgerMenuIcon className="h-5 w-5 text-muted-foreground animate-spin" />
           </ClerkLoading>
           <ClerkLoaded>
             <SignedIn>
               <UserButton />
             </SignedIn>
             <SignedOut>
-              <SignInButton mode='redirect' afterSignInUrl='/home' afterSignUpUrl='/home'>
-                <Button size='lg'>Entrar</Button>
+              <SignInButton
+                mode="redirect"
+                afterSignInUrl="/home"
+                afterSignUpUrl="/home"
+              >
+                <Button size="lg">Entrar</Button>
               </SignInButton>
             </SignedOut>
           </ClerkLoaded>
