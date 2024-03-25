1dd394e8096cfe1a54ab35b8b067612f768b187d
Vitor <tutornews2@gmail.com>
2024-03-25 13:23:59 -0300

    adiciona responsividade buttons

--- src/app/(application)/home/page.tsx
+++ src/app/(application)/home/page.tsx
@@ -3,7 +3,7 @@ import GroupButtons from "@/components/group_buttons";
 
 export default function Home() {
   return (
-    <main className="container p-16 pt-28 flex flex-col gap-6">
+    <main className="container sm:px-16 p-8 pt-28 flex flex-col gap-6">
       <GroupButtons />
       <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
         <Card />
--- src/components/group_buttons.tsx
+++ src/components/group_buttons.tsx
@@ -2,10 +2,10 @@ import { Button } from "@/components/ui/button";
 
 export default function GroupButtons() {
   return (
-    <div className="flex items-center gap-2">
-      <Button variant={"default"}>Disponíveis</Button>
-      <Button variant={"outline"}>Em andamento</Button>
-      <Button variant={"outline"}>Concluídos</Button>
+    <div className="flex items-center gap-2 max-[430px]:gap-1 flex-wrap">
+      <Button className="max-[430px]:text-xs" variant={"default"}>Disponíveis</Button>
+      <Button className="max-[430px]:text-xs" variant={"outline"}>Em andamento</Button>
+      <Button className="max-[430px]:text-xs" variant={"outline"}>Concluídos</Button>
     </div>
   );
 }
