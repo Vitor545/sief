d8157deb96626d670073f54f722b7dc6ae3be945
Vitor <tutornews2@gmail.com>
2024-03-31 00:55:17 -0300

    fix axios

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: '/api',
+  baseURL: process.env.NODE_ENV === 'production' ? 'https://sief-one.vercel.app/api' : 'http://localhost:3000/api',
 });
