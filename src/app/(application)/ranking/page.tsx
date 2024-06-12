4a99cccca4708af099ff64c5168345878b8f76b5
Vitor <tutornews2@gmail.com>
2024-06-12 19:50:50 -0300

    fix: adiciona contains any

--- src/app/(application)/ranking/page.tsx
+++ src/app/(application)/ranking/page.tsx
@@ -1,17 +1,27 @@
+'use client';
+
 import RankingContainer from "@/components/ranking_container";
 import RankingHero from "@/components/ranking_hero";
 import RankingList from "@/components/ranking_list";
 import { api } from "@/lib/axios";
+import { useEffect, useState } from "react";
 
 
-export default async function Ranking() {
-    const ranking = await api.get("/ranking");
+export default function Ranking() {
+    const [ranking, setRanking] = useState<any>([]);
+    const getRanking = async () => {
+        const response = await api.get("/ranking");
+        setRanking(response?.data || []);
+    }
+    useEffect(() => {
+        getRanking()
+    }, [])
     return (
         <main className="container sm:px-16 text-center p-8 pt-28 flex flex-col gap-6">
             <RankingHero />
             <RankingContainer>
                 {
-                    ranking?.data?.sort((el: any) => el?.pontos)?.map((el: any, index: number) => (
+                    ranking?.sort((el: any) => el?.pontos)?.map((el: any, index: number) => (
                         <RankingList key={el.imageurl} {...el} position={index} />
                     ))
                 }
